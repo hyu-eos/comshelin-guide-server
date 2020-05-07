b7a5b7544634db23103d00af984c7af8af763fe3
penguo <ireninpr@gmail.com>
2020-05-07 22:00:39 +0900

    restuarant delete 추가

--- routes/api/restaurant.js
+++ routes/api/restaurant.js
@@ -2,9 +2,21 @@ let router = require('express').Router();
 let dbHandler = require('../../common/handler')(false);
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/list', function (req, res, next) {
     dbHandler.dbConnect(req, res, function (conn, callback) {
         conn.query("SELECT * FROM restaurant", [], function (err, rows) {
+            if (err) {
+                callback(null, err);
+            }
+            callback(rows);
+        });
+    });
+});
+
+router.get('/', function (req, res, next) {
+    let id = req.query["id"];
+    dbHandler.dbConnect(req, res, function (conn, callback) {
+        conn.query("SELECT * FROM restaurant WHERE id = ?", [id], function (err, rows) {
             if (err) {
                 callback(null, err);
             }
@@ -17,6 +29,19 @@ router.post('/', function (req, res, next) {
     var value = req.body;
     dbHandler.dbConnect(req, res, function (conn, callback) {
         conn.query("INSERT INTO restaurant SET ?", value, function (err, rows) {
+            if (err) {
+                console.log(err);
+                callback(null, err);
+            }
+            callback(null, null);
+        });
+    });
+});
+
+router.delete('/', function (req, res, next) {
+    let id = req.query["id"];
+    dbHandler.dbConnect(req, res, function (conn, callback) {
+        conn.query("DELETE FROM restaurant WHERE id = ?", [id], function (err, rows) {
             if (err) {
                 console.log(err);
                 callback(null, err);
