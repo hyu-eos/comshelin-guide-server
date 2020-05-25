3118f07a4181c1136b2e74446edc3af9ae4f32e0
penguo <ireninpr@gmail.com>
2020-05-25 16:45:01 +0900

    Recipe 추가

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/api/restaurant', apiRestaurantRouter);
+app.use('/api/recipe', apiRestaurantRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/api/recipe.js
@@ -0,0 +1,54 @@
+let router = require('express').Router();
+let dbHandler = require('../../common/handler')(false);
+
+/* GET home page. */
+router.get('/list', function (req, res, next) {
+    dbHandler.dbConnect(req, res, function (conn, callback) {
+        conn.query("SELECT * FROM recipe", [], function (err, rows) {
+            if (err) {
+                callback(null, err);
+            }
+            callback(rows);
+        });
+    });
+});
+
+router.get('/', function (req, res, next) {
+    let id = req.query["id"];
+    dbHandler.dbConnect(req, res, function (conn, callback) {
+        conn.query("SELECT * FROM recipe WHERE id = ?", [id], function (err, rows) {
+            if (err) {
+                callback(null, err);
+            }
+            callback(rows);
+        });
+    });
+});
+
+router.post('/', function (req, res, next) {
+    var value = req.body;
+    dbHandler.dbConnect(req, res, function (conn, callback) {
+        conn.query("INSERT INTO recipe SET ?", value, function (err, rows) {
+            if (err) {
+                console.log(err);
+                callback(null, err);
+            }
+            callback(null, null);
+        });
+    });
+});
+
+router.delete('/', function (req, res, next) {
+    let id = req.query["id"];
+    dbHandler.dbConnect(req, res, function (conn, callback) {
+        conn.query("DELETE FROM recipe WHERE id = ?", [id], function (err, rows) {
+            if (err) {
+                console.log(err);
+                callback(null, err);
+            }
+            callback(null, null);
+        });
+    });
+});
+
+module.exports = router;
